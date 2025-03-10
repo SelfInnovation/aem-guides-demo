dfc1fccdb4d1b6d784ec731b9fd41df2b027ca1d
mkkol <mkkolusu@gmail.com>
2025-03-10 16:11:30 -0500

    Initial Commit

---
+++ core/src/main/java/com/adobe/aem/guides/demo/core/models/DemoModelClass.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.adobe.aem.guides.demo.core.models;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+/**
+ * @author mkkol
+ *
+ */
+
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class DemoModelClass {
+	
+	@Inject
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+}
