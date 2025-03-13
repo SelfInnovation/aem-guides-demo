5a2f17826f6abe65f98f49d76f6accbb2a870168
mkkol <mkkolusu@gmail.com>
2025-03-12 22:56:44 -0500

    Initial COmmit

---
+++ core/src/main/java/com/adobe/aem/guides/demo/core/schedulers/DemoSchedular.java
@@ -0,0 +1,22 @@
+package com.adobe.aem.guides.demo.core.schedulers;
+
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.Designate;
+
+
+@Designate(ocd=SimpleScheduledTask.Config.class)
+@Component(service = Runnable.class, immediate = true)
+public class DemoSchedular implements Runnable{
+
+	
+	
+	Scheduler schedular;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
